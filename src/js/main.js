86ebdf94744bc47add6655070134116901cfb66c
BasankoSV <basankosv@gmail.com>
2021-08-19 13:29:07 +0500

    Renamed script.js - main.js, added jquery.

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,33 @@
-$(document).ready(function () {
-  var floorPath = $(".home-image path")
-  var currentFloor = 2
-  var counterUp = $(".counter-up")
-  var counterDown = $(".counter-down")
-  floorPath.on("mouseover", function () {
-    floorPath.removeClass("current-floor")
-    currentFloor = $(this).attr('data-floor')
-    $(".counter").text(currentFloor)
-  })
+const floorPath = document.querySelectorAll(".home-image path")
+const counter = document.querySelector(".counter")
+const btnCounterUp = document.querySelector(".counter-up")
+const btnCounterDown = document.querySelector(".counter-down")
 
-  counterUp.on("click", function() {
-    if (currentFloor < 18) {
-      currentFloor++
-      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGroupping: false })
-      $(".counter").text(usCurrentFloor)
-      floorPath.removeClass("current-floor")
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
+const zero = num => {
+  if (num < 10) return num = "0" + num
+  else return num.toString()
+}
+const arrow = (btn, sign, condition) => {
+  btn.addEventListener('click', e => {
+    if (eval(condition)) {
+      sign === '+' ? currentFloor++ : currentFloor--
+      counter.textContent = zero(currentFloor)
+      floorPath.forEach(floor => {
+        floor.classList.contains("current-floor") && floor.classList.remove("current-floor")
+        if (floor.dataset.floor === zero(currentFloor)) floor.classList.toggle("current-floor")
+      })
     }
   })
+}
 
-  counterDown.on('click', function() {
-    if (currentFloor > 2) {
-      currentFloor--
-      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGroupping: false})
-      $(".counter").text(usCurrentFloor)
-      floorPath.removeClass("current-floor")
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
-    }
+let currentFloor = 2
+
+floorPath.forEach(floor => {
+  floor.addEventListener('mouseover', e => {
+    floorPath.forEach(floor => floor.classList.remove("current-floor"))
+    currentFloor = counter.textContent = e.target.dataset.floor
   })
 })
+
+arrow(btnCounterUp, '+', 'currentFloor < 18')
+arrow(btnCounterDown, '-', 'currentFloor > 2')
