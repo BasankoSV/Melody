aeeaca1d08ad0debb671fbdbe5b8b24ad1da021a
BasankoSV <basankosv@gmail.com>
2021-08-20 17:29:48 +0500

    Сверстано модальное окно и footer.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 const floorPath = document.querySelectorAll(".home-image path")
-const counter = document.querySelector(".counter")
+const counter = document.querySelectorAll(".counter")
+// const counter = document.querySelector(".counter")
 const btnCounterUp = document.querySelector(".counter-up")
 const btnCounterDown = document.querySelector(".counter-down")
+const modal = document.querySelector(".modal")
+const modalCloseBtn = document.querySelector(".modal-close-button")
+const btnPrimary = document.querySelector(".button-primary")
 
 const zero = num => {
   if (num < 10) return num = "0" + num
@@ -11,7 +15,10 @@ const arrow = (btn, sign, condition) => {
   btn.addEventListener('click', () => {
     if (eval(condition)) {
       sign === '+' ? currentFloor++ : currentFloor--
-      counter.textContent = zero(currentFloor)
+      for (let i = 0; i < counter.length; i++) {
+        counter[i].textContent = zero(currentFloor)
+      }
+      // counter.textContent = zero(currentFloor)
       floorPath.forEach(floor => {
         floor.classList.contains("current-floor") && floor.classList.remove("current-floor")
         if (floor.dataset.floor === zero(currentFloor)) floor.classList.toggle("current-floor")
@@ -20,12 +27,24 @@ const arrow = (btn, sign, condition) => {
   })
 }
 
+const toggleModal = () => {
+  modal.classList.toggle("is-open")
+}
+
 let currentFloor = 2
+
 floorPath.forEach(floor => {
   floor.addEventListener('mouseover', e => {
     floorPath.forEach(floor => floor.classList.remove("current-floor"))
-    currentFloor = counter.textContent = e.target.dataset.floor
+    for (let i = 0; i < counter.length; i++) {
+      currentFloor = counter[i].textContent = e.target.dataset.floor
+    }
+    // currentFloor = counter.textContent = e.target.dataset.floor
   })
 })
 arrow(btnCounterUp, '+', 'currentFloor < 18')
 arrow(btnCounterDown, '-', 'currentFloor > 2')
+
+floorPath.forEach(floor => floor.addEventListener('click', toggleModal))
+btnPrimary.addEventListener('click', toggleModal)
+modalCloseBtn.addEventListener('click', toggleModal)
